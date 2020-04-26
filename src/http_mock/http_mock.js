0aa6a075bd6218e4b9a3b9995d501a24d3d7adbb
JoaoPedro <jpgmoreira19@gmail.com>
2020-04-26 11:34:24 -0300

    Mocking recipe information http request.

--- src/http_mock/http_mock.js
+++ src/http_mock/http_mock.js
@@ -1,10 +1,19 @@
 const path = require('path');
 const nock = require('nock');
 
-// Mock any requests to a specific host:
+// Side note: mock any requests to a specific host:
 // https://github.com/nock/nock/issues/495#issuecomment-328501637
+
 nock('https://api.spoonacular.com')
 	.persist()
-	.get(() => true)
+	.get('/recipes/complexSearch')
+	.query(true)
 	.delay(500)
-	.replyWithFile(200, path.join(__dirname, 'responses/search.json') , {'Content-Type': 'application/json'});
+	.replyWithFile(200, path.join(__dirname, 'responses/search.json'), {'Content-Type': 'application/json'})
+
+nock('https://api.spoonacular.com')
+	.persist()
+	.get(/recipes\/\d*\/information$/)
+	.query(true)
+	.delay(500)
+	.replyWithFile(200, path.join(__dirname, 'responses/recipe.json'), {'Content-Type': 'application/json'});
