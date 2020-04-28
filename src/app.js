9d7f5b78db54db81679db86738a4220243941904
JoaoPedro <jpgmoreira19@gmail.com>
2020-04-28 13:22:39 -0300

    Routes moved from app.js to a separate router.js file.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
 const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
-const spoonacular = require('./utils/spoonacular');
-const appUtils = require('./utils/app_utils');
+const router = require('./routers/router');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,7 +11,7 @@ if (process.env.NODE_ENV === 'development') {
 	require('./http_mock/http_mock');  // Mock HTTP requests to external APIs.
 }
 
-// Paths:
+// Local paths:
 const pubDir = path.join(__dirname, '../public');
 const viewsPath = path.join(__dirname, '../templates/views');
 const partialsPath = path.join(__dirname, '../templates/partials');
@@ -29,103 +28,8 @@ app.use(express.static(pubDir));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// -------------------------------------------------------------------
-
-// Routes:
-app.get(['/', '/home'], (req, res) => {
-	res.render('index');
-});
-
-app.get('/about', (req, res) => {
-	res.render('about');
-});
-
-app.get('/signup', (req, res) => {
-	res.render('signup');
-});
-
-app.get('/signin', (req, res) => {
-	res.render('signin');
-});
-
-app.post('/signup', (req, res) => {
-	res.status(200).send(req.body);
-});
-
-app.post('/signin', (req, res) => {
-	res.status(200).send(req.body);
-});
-
-app.get('/search', (req, res) => {
-	const searchText = req.query.searchText;
-	const pageNumber = Math.max(req.query.pageNumber, 1);  // Prevents page number <= 0.
-	const resultsPerPage = 10;  // Spoonacular API allows a maximum of 10 results per request.
-	spoonacular.searchRequest(searchText, pageNumber, resultsPerPage)
-		.then((response) => {
-			response = response.data;
-			const totalResults = response.totalResults;
-			let pagination = false;  // If no results found, do not show pagination bar.
-			if (totalResults > 0) {
-				pagination = appUtils.searchPagination(searchText, pageNumber, resultsPerPage, totalResults);
-			}
-
-			// Filter results contents:
-			const results = [];
-			response.results.forEach((result) => {
-				const { id, title, image, summary, dishTypes } = result;
-				const newRes = {
-					id,
-					image,
-					title: appUtils.filterBadWords(title),
-					summary: appUtils.filterBadWords(summary),
-					dishTypes: dishTypes.map(appUtils.filterBadWords)
-				}
-				results.push(newRes);
-			});
-		
-			res.render('search', {
-				results,
-				searchText,
-				pagination
-			});		
-		})
-		.catch((error) => {
-			error = error.response.data;
-			res.render('error', {
-				message: spoonacular.errorMessage(error.code)
-			});
-		});
-});
-
-app.get('/recipe', (req, res) => {
-	spoonacular.recipeRequest(req.query.id)
-		.then((response) => {
-			response = response.data;
-			// analyzedInstructions is (by some reason) provided as an array with at most one object.
-			if (response.analyzedInstructions.length > 0) {
-				response.instructionsSteps = response.analyzedInstructions[0].steps;
-			}
-			else {
-				response.instructionsSteps = undefined;
-			}
-			response.hasSource = (response.sourceUrl || response.sourceName || response.creditsText);
-
-			// Since the recipe view uses several informations from the response object, the simpler approach
-			//  below was chosen instead of filtering each text property separately, such as it's done
-			//  in the search route above.
-			response = JSON.parse(appUtils.filterBadWords(JSON.stringify(response)));
-			
-			res.render('recipe', { response });
-		})
-		.catch((error) => {
-			error = error.response.data;
-			res.render('error', {
-				message: spoonacular.errorMessage(error.code)
-			});
-		});
-});
-
-// -------------------------------------------------------------------
+// Use router:
+app.use(router);
 
 // Start server:
 app.listen(port, () => {
---
+++ src/routers/router.js
@@ -0,0 +1,104 @@
+const express = require('express');
+const spoonacular = require('../utils/spoonacular');
+const appUtils = require('../utils/app_utils');
+
+const router = new express.Router();
+
+router.get(['/', '/home'], (req, res) => {
+	res.render('index');
+});
+
+router.get('/about', (req, res) => {
+	res.render('about');
+});
+
+// - SignIn and SignUp:
+
+router.get('/signup', (req, res) => {
+	res.render('signup');
+});
+
+router.get('/signin', (req, res) => {
+	res.render('signin');
+});
+
+router.post('/signup', (req, res) => {
+	res.status(200).send(req.body);
+});
+
+router.post('/signin', (req, res) => {
+	res.status(200).send(req.body);
+});
+
+//
+
+router.get('/search', (req, res) => {
+	const searchText = req.query.searchText;
+	const pageNumber = Math.max(req.query.pageNumber, 1);  // Prevents page number <= 0.
+	const resultsPerPage = 10;  // Spoonacular API allows a maximum of 10 results per request.
+	spoonacular.searchRequest(searchText, pageNumber, resultsPerPage)
+		.then((response) => {
+			response = response.data;
+			const totalResults = response.totalResults;
+			let pagination = false;  // If no results found, do not show pagination bar.
+			if (totalResults > 0) {
+				pagination = appUtils.searchPagination(searchText, pageNumber, resultsPerPage, totalResults);
+			}
+
+			// Filter results contents:
+			const results = [];
+			response.results.forEach((result) => {
+				const { id, title, image, summary, dishTypes } = result;
+				const newRes = {
+					id,
+					image,
+					title: appUtils.filterBadWords(title),
+					summary: appUtils.filterBadWords(summary),
+					dishTypes: dishTypes.map(appUtils.filterBadWords)
+				}
+				results.push(newRes);
+			});
+		
+			res.render('search', {
+				results,
+				searchText,
+				pagination
+			});		
+		})
+		.catch((error) => {
+			error = error.response.data;
+			res.render('error', {
+				message: spoonacular.errorMessage(error.code)
+			});
+		});
+});
+
+router.get('/recipe', (req, res) => {
+	spoonacular.recipeRequest(req.query.id)
+		.then((response) => {
+			response = response.data;
+			// analyzedInstructions is (by some reason) provided as an array with at most one object.
+			if (response.analyzedInstructions.length > 0) {
+				response.instructionsSteps = response.analyzedInstructions[0].steps;
+			}
+			else {
+				response.instructionsSteps = undefined;
+			}
+			response.hasSource = (response.sourceUrl || response.sourceName || response.creditsText);
+
+			// Since the recipe view uses several informations from the response object, the simpler approach
+			//  below was chosen instead of filtering each text property separately, such as it's done
+			//  in the search route above.
+			response = JSON.parse(appUtils.filterBadWords(JSON.stringify(response)));
+			
+			res.render('recipe', { response });
+		})
+		.catch((error) => {
+			error = error.response.data;
+			res.render('error', {
+				message: spoonacular.errorMessage(error.code)
+			});
+		});
+});
+
+module.exports = router;
