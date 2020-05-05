76861fb72ead02a09833c261c19eef15733d6fc4
JoaoPedro <jpgmoreira19@gmail.com>
2020-05-04 23:46:05 -0300

    Added environment variable with the Atlas MongoDB connection URL.

--- src/db/connect.js
+++ src/db/connect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionUrl = 'mongodb://127.0.0.1:27017/dbtest1';
+const connectionUrl = process.env.MONGODB_URL;
 
 mongoose.connect(connectionUrl, {
 	useNewUrlParser: true,
