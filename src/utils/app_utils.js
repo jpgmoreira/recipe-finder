b806066e2efc1ca6cebf7f61dde0aa2101b05e51
JoaoPedro <jpgmoreira19@gmail.com>
2020-05-09 12:36:22 -0300

    Corrected pagination nav overflow on search results page.

--- src/utils/app_utils.js
+++ src/utils/app_utils.js
@@ -9,10 +9,12 @@ filter.addWords(...customBadWords);
  * Compute the content of the buttons on the page navigation bar of the search results page.
  * Returns an array of objects with the properties:
  * {
- *   text: string. button's text (page number, 'Previous' or 'Next'),
- *   current: boolean. true if it is the current page.
- * 	 blocked: boolean. true if the button cannot be clicked.
- *   dest: string. href to the page destination of the button.
+ *   -- text: string. button's text (page number, '<' or '>'),
+ *   -- current: boolean. true if it is the current page.
+ * 	 -- blocked: boolean. true if the button cannot be clicked.
+ *   -- dest: string. href to the page destination of the button.
+ * 	 -- required: string. 'pag-required' if the button will be displayed 
+ *        regardless of the device width. 'pag-not-required' otherwise.
  * }
  */
 const searchPagination = (searchText, pageNumber, resultsPerPage, totalResults) => {
@@ -41,24 +43,27 @@ const searchPagination = (searchText, pageNumber, resultsPerPage, totalResults) 
 	const nextPage = (currPage === maxPage) ? maxPage : (currPage + 1);
 
 	let pages = [{
-		text: 'Previous',
+		text: '<',
 		current: false,
 		blocked: (currPage === 1),
-		dest: `/search?searchText=${searchText}&pageNumber=${previousPage}`
+		dest: `/search?searchText=${searchText}&pageNumber=${previousPage}`,
+		required: 'required'
 	}];
 	for (let i = low; i <= high; i++) {
 		pages.push( {
 			text: String(i),
 			current: (i === currPage),
 			blocked: false,
-			dest: `/search?searchText=${searchText}&pageNumber=${i}`
+			dest: `/search?searchText=${searchText}&pageNumber=${i}`,
+			required: (Math.abs(currPage - i) <= 1) ? 'pag-required' : 'pag-not-required'
 		});
 	}
 	pages.push({
-		text: 'Next',
+		text: '>',
 		current: false,
 		blocked: (currPage === maxPage),
-		dest: `/search?searchText=${searchText}&pageNumber=${nextPage}`
+		dest: `/search?searchText=${searchText}&pageNumber=${nextPage}`,
+		required: 'required'
 	});
 
 	return pages;
